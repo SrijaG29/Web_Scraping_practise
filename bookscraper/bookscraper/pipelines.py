39206366bb2d8e8056d46a992d0d311d40a8ca99
SrijaG29 <srijag298@gmail.com>
2024-07-18 11:06:30 +0000

    Cleaned data for availability colomn.

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -10,4 +10,15 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        # extracting the no of items avilable instead of whole string
+        val = adapter.get('availability')
+        val = val[0]
+        split_val = val.split('(')
+        if len(split_val) < 2:
+            adapter['availability'] = 0
+        else:
+            num_val = split_val[1].split(' ')
+            adapter['availability'] = int(num_val[0])
         return item
+    
